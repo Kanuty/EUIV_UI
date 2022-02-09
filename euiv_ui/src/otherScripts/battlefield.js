15ed687891bb7cc6a6a48b4c595683ad5a55c6c2
Kanuty <bartoszdudek2211@gmail.com>
2022-02-09 23:51:41 +0100

    battlefield -02
    add possibility to put soldiers on battlefield

--- euiv_ui/src/otherScripts/battlefield.js
+++ euiv_ui/src/otherScripts/battlefield.js
@@ -58,18 +58,22 @@ class Army {
       this.infantryUnits[i] = new Unit();
     }
     for (let i = 0; i < cavUnits; i++) {
-      this.cavaleryUnits[i] = new Unit({type: "cavalery"});
+      this.cavaleryUnits[i] = new Unit({ type: "cavalery" });
     }
     for (let i = 0; i < artUnits; i++) {
-      this.artyleryUnits[i] = new Unit({type: "artylery"});
+      this.artyleryUnits[i] = new Unit({ type: "artylery" });
     }
   }
 }
 
 class BattleField {
-  usableBattleWidth;
+  usableBattleWidth = 15;
   lineMele;
   lineRange;
+  reservs = [];
+  #middleOfBattleLine = Math.ceil(globalConstants.battleLineLength / 2 );
+  #middleMinusHalfOfBattleWidth;
+  #middlePlusHalfOfBattleWidth;
   constructor(battleWidth = 15) {
     this.usableBattleWidth = battleWidth;
     this.lineMele = new Array(globalConstants.battleLineLength);
@@ -80,21 +84,18 @@ class BattleField {
     for (let i = 0; i < this.lineRange.length; i++) {
       this.lineRange[i] = new BattleBox();
     }
+    this.#middleMinusHalfOfBattleWidth = Math.floor(
+      this.#middleOfBattleLine - Math.floor(this.usableBattleWidth / 2)
+    );
+    this.#middlePlusHalfOfBattleWidth = Math.floor(
+      this.#middleOfBattleLine + Math.floor(this.usableBattleWidth / 2)
+    );
     this.SetAvailableBattleBoxes();
   }
   SetAvailableBattleBoxes() {
-    const middleMinusHalfOfBattleWidth = Math.floor(
-      globalConstants.battleLineLength / 2 -
-        Math.floor(this.usableBattleWidth / 2)
-    );
-    const middlePlusHalfOfBattleWidth = Math.floor(
-      globalConstants.battleLineLength / 2 +
-        Math.floor(this.usableBattleWidth / 2)
-    );
-    //  console.log(middleMinusHalfOfBattleWidth, middlePlusHalfOfBattleWidth)
     for (
-      let i = middleMinusHalfOfBattleWidth;
-      i < middlePlusHalfOfBattleWidth;
+      let i = this.#middleMinusHalfOfBattleWidth;
+      i < this.#middlePlusHalfOfBattleWidth;
       i++
     ) {
       this.lineMele[i].blocked = false;
@@ -102,5 +103,65 @@ class BattleField {
     }
   }
 
-  fillBattleFieldWithUnits() {}
+  fillBattleFieldWithUnits(army) {
+    //for now ths is scenario for infantry only
+    const infantryUnitsLength = army.infantryUnits.length;
+    if (infantryUnitsLength <= this.usableBattleWidth) {
+      let infantryIndex = 0;
+      for (
+        let i = Math.floor(this.#middleOfBattleLine - infantryUnitsLength / 2);
+        i < Math.floor(this.#middleOfBattleLine + infantryUnitsLength / 2);
+        i++
+      ) {
+        this.lineMele[i].entity = army.infantryUnits[infantryIndex];
+        infantryIndex++;
+      }
+    } else if (infantryUnitsLength > this.usableBattleWidth) {
+      let infantryIndex = 0;
+      for (
+        let i = this.#middleMinusHalfOfBattleWidth;
+        i < this.#middlePlusHalfOfBattleWidth;
+        i++
+      ) {
+        this.lineMele[i].entity = army.infantryUnits[infantryIndex];
+        infantryIndex++;
+      }
+      if(infantryUnitsLength * 2 <= this.usableBattleWidth ){
+      for (
+        let i = Math.floor(this.#middleOfBattleLine - (infantryUnitsLength - this.usableBattleWidth)/ 2);
+        i < Math.floor(this.#middleOfBattleLine + (infantryUnitsLength - this.usableBattleWidth) / 2);
+        i++
+      ) {
+        this.lineRange[i].entity = army.infantryUnits[infantryIndex];
+        infantryIndex++;
+      }
+    } else if(infantryUnitsLength * 2 > this.usableBattleWidth ){
+      for (
+        let i = this.#middleMinusHalfOfBattleWidth;
+        i < this.#middlePlusHalfOfBattleWidth;
+        i++
+      ) {
+        this.lineRange[i].entity = army.infantryUnits[infantryIndex];
+        infantryIndex++;
+      }
+    }
+        for (let i = infantryIndex++; i < infantryUnitsLength; i++) {
+          this.reservs.push(army.infantryUnits[i]);
+        }
+    }
+  }
 }
+var a3 = new Army(10, 0, 0);
+var b3 = new BattleField();
+b3.fillBattleFieldWithUnits(a3);
+console.log(b3);
+
+ var a1 = new Army(25, 0, 0);
+ var b1 = new BattleField();
+ b1.fillBattleFieldWithUnits(a1);
+ console.log(b1);
+
+var a2 = new Army(50, 0, 0);
+var b2 = new BattleField();
+b2.fillBattleFieldWithUnits(a2);
+console.log(b2);
